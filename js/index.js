56f6e5f97c47932dd7a904651931b1059f0cf136
Randy-RM <randymuhema@gmail.com>
2024-11-27 08:35:31 +0000

    feat : creat basic navigation handler

--- js/index.js
+++ js/index.js
@@ -2,6 +2,20 @@
 const burgerMenu = document.querySelector("#burger-menu");
 const sideBarMenu = document.querySelector("#side-bar");
 const mainSection = document.querySelector("#main-section");
+//
+const addContactButtonList = document.querySelectorAll(".addContactButton");
+const arrowBack = document.getElementById("arrowBack");
+//
+const contactListContainer = document.getElementById("contactListContainer");
+const emptyContactList = document.getElementById("emptyContactList");
+const contactList = document.getElementById("contactList");
+//
+const contactFormContainer = document.getElementById("contactFormContainer");
+const contactFirstName = document.getElementById("contactFirstName");
+const contactName = document.getElementById("contactName");
+const saveContact = document.getElementById("saveContact");
+
+const contacts = [];
 
 // Callback functions
 const handleAddStyle = (element, style) => {
@@ -10,6 +24,28 @@ const handleAddStyle = (element, style) => {
 
 const handleRemoveStyle = (element, style) => {
   element.classList.remove(style);
+};
+
+const goBack = () => {
+  handleRemoveStyle(contactFormContainer, "display-visible");
+  handleAddStyle(contactFormContainer, "display-none");
+  //
+  handleRemoveStyle(contactListContainer, "display-none");
+  handleAddStyle(contactListContainer, "display-visible");
+
+  if (contacts.length > 0) {
+    handleRemoveStyle(emptyContactList, "display-visible");
+    handleAddStyle(emptyContactList, "display-none");
+    //
+    handleRemoveStyle(contactList, "display-none");
+    handleAddStyle(contactList, "display-visible");
+  } else {
+    handleRemoveStyle(contactList, "display-visible");
+    handleAddStyle(contactList, "display-none");
+    //
+    handleRemoveStyle(emptyContactList, "display-none");
+    handleAddStyle(emptyContactList, "display-visible");
+  }
 };
 
 // Add events
@@ -22,4 +58,35 @@ burgerMenu.addEventListener("click", (event) => {
     handleAddStyle(sideBarMenu, "display-none");
     handleAddStyle(mainSection, "w-100");
   }
+});
+
+addContactButtonList.forEach((addContactButton) => {
+  addContactButton.addEventListener("click", () => {
+    handleRemoveStyle(contactListContainer, "display-visible");
+    handleAddStyle(contactListContainer, "display-none");
+    //
+    handleRemoveStyle(contactFormContainer, "display-none");
+    handleAddStyle(contactFormContainer, "display-visible");
+  });
+});
+
+arrowBack.addEventListener("click", () => {
+  goBack();
+});
+
+saveContact.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const contact = {
+    contactFirstName: contactFirstName.value,
+    contactName: contactName.value,
+  };
+
+  contacts.push(contact);
+
+  contactFirstName.value = "";
+  contactName.value = "";
+
+  console.log(contacts);
+  goBack();
 });
